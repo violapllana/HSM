3c6d0632b519293ba1cd0425c377f90d64d99e21
violapllana <violapllana11@gmail.com>
2025-04-17 23:21:20 +0200

    Admini mundet me shtu connection ne mes doktorit dhe pacientit

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 const Report = require('./report');
+const DoctorPatient = require('./doctorpatient');
 
 // Krijimi i lidhjeve
 Report.belongsTo(User, { as: 'doctor', foreignKey: 'doctorId' });
@@ -8,7 +9,25 @@ Report.belongsTo(User, { as: 'patient', foreignKey: 'patientId' });
 User.hasMany(Report, { as: 'doctorReports', foreignKey: 'doctorId' });
 User.hasMany(Report, { as: 'patientReports', foreignKey: 'patientId' });
 
+
+
+// Lidhja shumë-në-shumë midis mjekut dhe pacientit
+User.belongsToMany(User, {
+  through: DoctorPatient,
+  foreignKey: 'doctorId',
+  otherKey: 'patientId',
+  as: 'patients',
+});
+User.belongsToMany(User, {
+  through: DoctorPatient,
+  foreignKey: 'patientId',
+  otherKey: 'doctorId',
+  as: 'doctors',
+});
+
+
 module.exports = {
   User,
   Report,
+  DoctorPatient,
 };
--- backend/routes/connectRoutes.js
+++ backend/routes/connectRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { User } = require('../models/user');
+const User = require('../models/user');
+const DoctorPatient = require('../models/doctorpatient');  // Për modelin e lidhjes
 const router = express.Router();
-
 /**
  * @swagger
- * /connect:  
+ * tags:
+ *   name: Connection
+ *   description: Menaxhimi i lidhjeve mes mjekëve dhe pacientëve
+ */
+/**
+ * @swagger
+ * /connect:
  *   post:
  *     summary: Krijo një lidhje mes mjekut dhe pacientit
  *     description: Kjo rrugë mundëson lidhjen mes një mjeku dhe një pacienti.
@@ -30,14 +36,13 @@ const router = express.Router();
  *       500:
  *         description: Gabim në server
  */
-router.post('/connect', async (req, res) => {
+router.post('/', async (req, res) => {
   const { doctorId, patientId } = req.body;
 
   try {
     const doctor = await User.findByPk(doctorId);
     const patient = await User.findByPk(patientId);
 
-    // Kontrollo nëse të dy përdoruesit ekzistojnë dhe nëse janë të përkatshëm
     if (!doctor || doctor.role !== 'doctor') {
       return res.status(400).json({ message: 'Invalid doctor.' });
     }
@@ -46,10 +51,169 @@ router.post('/connect', async (req, res) => {
       return res.status(400).json({ message: 'Invalid patient.' });
     }
 
-    // Mund të shtosh një relacion të ri mes mjekut dhe pacientit këtu nëse do
-    doctor.addPatient(patient); // Shembuj i një lidhjeje të mundshme
+    // Krijo lidhjen
+    await doctor.addPatient(patient);
 
     return res.status(200).json({ message: 'Successfully connected doctor and patient.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+/**
+ * @swagger
+ * /connect:
+ *   get:
+ *     summary: Merr të gjitha lidhjet mes mjekëve dhe pacientëve
+ *     description: Kjo rrugë mundëson marrjen e të gjitha lidhjeve mes mjekëve dhe pacientëve.
+ *     responses:
+ *       200:
+ *         description: Lidhjet mes mjekëve dhe pacientëve u morën me sukses
+ *       500:
+ *         description: Gabim në server
+ */
+router.get('/', async (req, res) => {
+  try {
+    const connections = await DoctorPatient.findAll();
+    
+    // Kontrollo nëse ka lidhje
+    if (!connections || connections.length === 0) {
+      return res.status(404).json({ message: 'No connections found.' });
+    }
+
+    res.status(200).json(connections);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+/**
+ * @swagger
+ * /connect/{id}:
+ *   get:
+ *     summary: Merr një lidhje mes mjekut dhe pacientit me ID
+ *     description: Kjo rrugë mundëson marrjen e lidhjes mes një mjeku dhe një pacienti bazuar në ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID e lidhjes mes mjekut dhe pacientit
+ *     responses:
+ *       200:
+ *         description: Lidhja mes mjekut dhe pacientit u gjet me sukses
+ *       404:
+ *         description: Lidhja nuk u gjet
+ *       500:
+ *         description: Gabim në server
+ */
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const connection = await DoctorPatient.findByPk(id);
+    if (!connection) {
+      return res.status(404).json({ message: 'Connection not found.' });
+    }
+
+    res.status(200).json(connection);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+/**
+ * @swagger
+ * /connect/{id}:
+ *   put:
+ *     summary: Përditëso një lidhje mes mjekut dhe pacientit
+ *     description: Kjo rrugë mundëson përditësimin e lidhjes mes një mjeku dhe një pacienti bazuar në ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID e lidhjes mes mjekut dhe pacientit
+ *       - in: body
+ *         name: connect
+ *         description: Lidhja që do të përditësohet
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             doctorId:
+ *               type: integer
+ *               description: ID e mjekut
+ *             patientId:
+ *               type: integer
+ *               description: ID e pacientit
+ *     responses:
+ *       200:
+ *         description: Lidhja u përditësua me sukses
+ *       400:
+ *         description: Lidhja nuk mund të përditësohet
+ *       404:
+ *         description: Lidhja nuk u gjet
+ *       500:
+ *         description: Gabim në server
+ */
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { doctorId, patientId } = req.body;
+
+  try {
+    const connection = await DoctorPatient.findByPk(id);
+
+    if (!connection) {
+      return res.status(404).json({ message: 'Connection not found.' });
+    }
+
+    // Përditëso lidhjen
+    connection.doctorId = doctorId;
+    connection.patientId = patientId;
+
+    await connection.save();
+
+    return res.status(200).json({ message: 'Connection updated successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+/**
+ * @swagger
+ * /connect/{id}:
+ *   delete:
+ *     summary: Fshi një lidhje mes mjekut dhe pacientit
+ *     description: Kjo rrugë mundëson fshirjen e lidhjes mes një mjeku dhe një pacienti bazuar në ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID e lidhjes mes mjekut dhe pacientit
+ *     responses:
+ *       200:
+ *         description: Lidhja u fshi me sukses
+ *       404:
+ *         description: Lidhja nuk u gjet
+ *       500:
+ *         description: Gabim në server
+ */
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const connection = await DoctorPatient.findByPk(id);
+
+    if (!connection) {
+      return res.status(404).json({ message: 'Connection not found.' });
+    }
+
+    await connection.destroy();
+
+    return res.status(200).json({ message: 'Connection deleted successfully.' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error.' });
