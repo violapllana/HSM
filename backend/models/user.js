54e563f1afd3f519faa8f261532162923a5b4201
reajasharii <reajashari88@gmail.com>
2025-06-30 23:51:29 +0200

    .

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,47 +1,4 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../db'); 
 
-// const User = sequelize.define('User', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//     allowNull: false,
-//   },
-//   username: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//     unique: true,
-//     validate: {
-//       len: { args: [3, 20], msg: "Username must be between 3 and 20 characters." }
-//     }
-//   },
-//   email: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//     unique: true,
-//     validate: {
-//       isEmail: { msg: "Must be a valid email address." }
-//     }
-//   },
-//   password: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//     validate: {
-//       len: {
-//         args: [8, 100],
-//         msg: "Password must be at least 8 characters long."
-//       }
-//     }
-//   },
-//   role: {
-//     type: DataTypes.ENUM('patient', 'doctor', 'admin'),
-//     defaultValue: 'patient',
-//     allowNull: false,
-//   }
-// });
-
-// module.exports = User;
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
